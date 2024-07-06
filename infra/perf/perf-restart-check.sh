d60ab0ae24f932c3f8f608459c17cc05bcd73299
Oliver Mannion <125105+tekumara@users.noreply.github.com>
2024-07-06 21:05:04 +1000

    feat: add delete workload to script

--- infra/perf/perf-restart-check.sh
+++ infra/perf/perf-restart-check.sh
@@ -2,6 +2,9 @@
 
 set -euo pipefail
 
+[[ "$#" -eq 0 || ("$1" != "upsert" && "$1" != "delete") ]] && \
+    echo -e "Restart a node whilst running the perf upsert or delete workload\n\nUsage: $0 upsert/delete" >&2 && exit 42
+
 cleanup() {
     local exit_status="$?"
     kill "$k6_pid"
@@ -10,8 +13,13 @@ cleanup() {
     exit "${exit_status}"
 }
 
-echo Start perf workload
-k6 run --duration 60s infra/perf/k6.js &
+echo Start "$1" workload
+if [[ "$1" == "upsert" ]]; then
+    k6 run --duration 60s infra/perf/k6.js &
+fi
+if [[ "$1" == "delete" ]]; then
+    DELETE_POINTS=true k6 run --duration 60s infra/perf/k6.js &
+fi
 k6_pid=$!
 
 # delete a pod and run the healthcheck
@@ -22,6 +30,11 @@ do
     sleep 5
     kubectl delete pod qdrant-1
     sleep 10
-    .venv/bin/python -m src.demo.healthcheck --assert-counts
+    if [[ "$1" == "upsert" ]]; then
+        .venv/bin/python -m src.demo.healthcheck --assert-counts
+    fi
+    if [[ "$1" == "delete" ]]; then
+        .venv/bin/python -m src.demo.healthcheck --assert-payload
+    fi
 done
 cleanup
